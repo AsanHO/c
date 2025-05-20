e6c219d29e82ecd2cd04cd4bffb4462fc9a0a89c
AsanHO <gorma00@naver.com>
2025-05-20 17:58:55 +0900

    cr : 7-10.stack_heap

---
+++ 7.function/10.stack_heap.cpp
@@ -0,0 +1,14 @@
+/*
+10.stack_heap
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << (141000 + 15000 + 26500 + 27200) / 5 << endl;
+    cout << (29000) / 3 << endl;
+    cout << 41900 + 9400 << endl;
+    return 0;
+}
