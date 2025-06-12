4328339a590341b67403ad79c38fe91d79f18061
AsanHO <eunho@ieunhoui-iMac.local>
2025-06-12 12:30:32 +0900

    rft

--- 11.inheritance/2.inheritance2/Student.h
+++ 11.inheritance/2.inheritance2/Student.h
@@ -19,7 +19,7 @@ class Student : public Person {
     // 상속과 템플릿을 배우면 코딩량이 적어진다. 뒤로갈수록 코딩 속력이 더
     // 빨라진다.
 
-    friend std::ostream& operator<<(std::ostream& out, Student& student) {
+    friend std::ostream& operator<<(std::ostream& out, const Student& student) {
         out << student.getName() << " " << student.m_intel;
         return out;
     }
