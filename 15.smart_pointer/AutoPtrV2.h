db751da6aa9051b5fff137d993609601979bdea6
AsanHO <eunho@ieunhoui-iMac.local>
2025-07-28 15:31:44 +0900

    cr : 15-3.move_constructors_assignment.cpp

---
+++ 15.smart_pointer/AutoPtrV2.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+
+// std::auto_ptr<int>; // c++98, c++ 11 deprecated 모던 c++에서는 쓰지 않음
+
+template <class T>
+class AutoPtrV2 {
+   public:
+    T *m_ptr = nullptr;
+
+   public:
+    AutoPtrV2(T *ptr = nullptr) : m_ptr(ptr) {
+        std::cout << "AutoPtrV2 default constructor" << std::endl;
+    }
+    ~AutoPtrV2() {
+        std::cout << "AutoPtrV2 destructor" << std::endl;
+        if (m_ptr != nullptr) delete m_ptr;
+    }
+
+    // AutoPtrV2(AutoPtrV2 &a) {
+    //     std::cout << "AutoPtrV2 copy constructor" << std::endl;
+    //     // m_ptr = a.m_ptr;
+
+    //     // deep copy
+    //     m_ptr = new T;
+    //     *m_ptr = *a.m_ptr;
+    // }
+
+    // AutoPtrV2 &operator=(const AutoPtrV2 &a) {
+    //     std::cout << "AutoPtrV2 copy assignment" << std::endl;
+    //     if (&a == this) return *this;
+
+    //     // delete m_ptr;
+    //     // m_ptr = a.m_ptr;
+    //     // a.m_ptr = nullptr;
+    //     if (m_ptr != nullptr) delete m_ptr;
+    //     m_ptr = new T;
+    //     *m_ptr = *a.m_ptr;
+    //     return *this;
+    // }
+
+    // move assignment
+    /*
+    &&로 잡은 매개변수는 메모리에 자리잡은게 아니라 잠깐 생겼다 사라질 의미가
+    된다.
+    */
+    AutoPtrV2(AutoPtrV2 &&a) : m_ptr(a.m_ptr) {
+        a.m_ptr = nullptr;  // R-value라서 굳이 필요하진 않지만 깔끔하니까!
+
+        std::cout << "AutoPtr move constructor" << std::endl;
+    }
+
+    AutoPtrV2 &operator=(AutoPtrV2 &&a) {
+        std::cout << "AutoPtr move assignment" << std::endl;
+        if (&a == this) return *this;
+        if (!m_ptr) delete m_ptr;
+
+        // shallow copy
+        m_ptr = a.m_ptr;
+        a.m_ptr = nullptr;  // R-value라서 굳이 필요하진 않지만 깔끔하니까!
+
+        return *this;
+    }
+    T &operator*() const { return *m_ptr; }
+    T *operator->() const { return m_ptr; }
+    bool isNull() const { return m_ptr == nullptr; }
+};
---
+++ 15.smart_pointer/ResourceV2.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+
+class ResourceV2 {
+   public:
+    int *m_data = nullptr;
+    unsigned m_length = 0;
+
+   public:
+    ResourceV2() { std::cout << "ResourceV2 constructed" << std::endl; }
+    ResourceV2(unsigned length) {
+        std::cout << "ResourceV2 constructed" << std::endl;
+        this->m_data = new int[length];
+        this->m_length = length;
+    }
+
+    ResourceV2(const ResourceV2 &res) {
+        std::cout << "ResourceV2 constructed" << std::endl;
+        ResourceV2(res.m_length);
+        for (unsigned i = 0; i < m_length; ++i) m_data[i] = res.m_data[i];
+    }
+    ~ResourceV2() {
+        std::cout << "ResourceV2 destructed" << std::endl;
+        if (m_data != nullptr) delete[] m_data;
+    }
+    ResourceV2 &operator=(ResourceV2 &res) {
+        std::cout << "ResourceV2 copy assignment" << std::endl;
+        if (&res == this) return *this;
+        if (this->m_data != nullptr) delete[] m_data;
+        m_length = res.m_length;
+        m_data = new int[m_length];
+
+        for (unsigned i = 0; i < m_length; ++i) m_data[i] = res.m_data[i];
+
+        return *this;
+    }
+
+    void print() {
+        for (unsigned i = 0; i < m_length; ++i) {
+            std::cout << m_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
